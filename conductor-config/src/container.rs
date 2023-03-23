6f19a6508827043c7ebc7f45f022381ccd61172c
Patrick Barrett <patrick@auxon.io>
2023-03-23 11:41:12 -0500

    just use the word container

--- conductor-config/src/container.rs
+++ conductor-config/src/container.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, Serialize, Deserialize)]
 #[serde(default, rename_all = "kebab-case")]
-pub struct DockerMachineProvider {
+pub struct ContainerMachineProvider {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub image: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
